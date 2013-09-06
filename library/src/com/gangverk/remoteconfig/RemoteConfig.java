13360471dad5d17524ddde5aa177f53dd4e4bf1f
Hörður Hauksson <hordur.hauks@gmail.com>
2013-09-06 13:14:35 +0000

    Updating remoteconfig

--- library/src/com/gangverk/remoteconfig/RemoteConfig.java
+++ library/src/com/gangverk/remoteconfig/RemoteConfig.java
@@ -1,8 +1,12 @@
 package com.gangverk.remoteconfig;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Iterator;
+import java.util.Locale;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -12,75 +16,151 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.os.AsyncTask;
-import android.preference.PreferenceManager;
+import android.util.Log;
 
+import com.gangverk.remoteconfig.R;
 public class RemoteConfig {
 	public static final String LAST_DOWNLOADED_CONFIG_KEY = "lastDownloadedConfig";
-	private Context context;
-	private String configLocation;
-	private long updateTime;
-	private HashMap<String, String> availableMaps;
-	private SharedPreferences preferences;
-	OnDownloadedConfigListener onDownloadedConfigListener  = null; 
+	private static final String TAG = "RemoteConfig";
+	private static final String PREFERENCE_NAME = "RemoteConfig";
+	// This is just a dot, since we have regular expression we have to have the backslashes as well
+	private static final String DEEP_DICTIONARY_SEPARATOR_REGEX = "\\."; 
+	private static final String DEEP_DICTIONARY_SEPARATOR = "."; 
+	private static final String REMOTE_CONFIG_INITIALIZER = "sp_has_initialized_rc"; 
+	private static final String REMOTE_CONFIG_FILE = "rc.json";
+	private static final String SP_VERSION_KEY = "rc_version";
+	private String mConfigLocation;
+	private long mUpdateTime;
+	private SharedPreferences mPreferences;
+	private Context mContext;
+	private RemoteConfigListener mListener  = null; 
 
-	public RemoteConfig(Context context) {
-		this(context, null, null);
-	}
+	public RemoteConfig() {}
 
-	public RemoteConfig(Context context, String configLocation, HashMap<String, String> availableMaps) {
-		this(context, configLocation, availableMaps, 3600);
-	}
-
-	public RemoteConfig(Context context, String configLocation, HashMap<String, String> availableMaps, long updateTime) {
-		this.context = context;
-		this.preferences = PreferenceManager.getDefaultSharedPreferences(context);
-		this.updateTime = updateTime*1000; // Convert to seconds
-		if(configLocation != null && availableMaps != null) {
-			this.configLocation = configLocation;
-			this.availableMaps = availableMaps;
-			InitializeClass();
+	public synchronized void init(Context context, int version) {
+		mContext = context;
+		mConfigLocation = mContext.getString(R.string.rc_config_location);
+		mUpdateTime = mContext.getResources().getInteger(R.integer.rc_config_update_interval);
+		mPreferences = mContext.getSharedPreferences(PREFERENCE_NAME, Context.MODE_PRIVATE);
+		int oldVersion = mPreferences.getInt(SP_VERSION_KEY, -1);
+		if(!mPreferences.getBoolean(REMOTE_CONFIG_INITIALIZER, false) || version>oldVersion) {
+			initializeConfigFile();
+			mPreferences.edit().putInt(SP_VERSION_KEY, version).apply();
 		}
 	}
 
-	private void InitializeClass () {
-		// Iterate through availablemaps and put into userdefaults if there is nothing there to begin with		
-		Editor editor = this.preferences.edit();
-		ArrayList<String> keys = new ArrayList<String>();
-		for (Map.Entry<String, String> entry : availableMaps.entrySet()) {
-			String key = entry.getKey();
-			String value = entry.getValue();
-			if(!this.preferences.contains(key)) {
-				editor.putString(key, value);
-			} else {
-				if(!this.preferences.getString(key, null).equals(value)) {
-					keys.add(key);
+	private void initializeConfigFile() {
+		// Start with parsing the assets/rc.json file into JSONObject
+		JSONObject remoteConfig = initialFileToJsonObject();
+		if(remoteConfig!=null) {
+			jsonObjectIntoPreferences(remoteConfig, true);
+		} else {
+			throw new RuntimeException("Unable to read rc.json file. Are you sure it exists in the assets folder?");
+		}
+		Editor editor = mPreferences.edit();
+		editor.putBoolean(REMOTE_CONFIG_INITIALIZER, true);
+		editor.apply();
+		checkForUpdate(); // We'll fetch new config on launch
+	}
+
+	private synchronized void jsonObjectIntoPreferences(final JSONObject jsonObject, boolean initial) {
+		Editor editor = mPreferences.edit();
+		ArrayList<String> changedKeys = new ArrayList<String>();
+		ArrayList<String> allKeys = getAllKeysFromJSONObject(jsonObject, null);
+		for(String newKey : allKeys) {
+
+			// If the key is inside an inner JSON dictionary it is defined with
+			// a dot like dictionary1.dictionary2. That's why we split the string 
+			// here
+			String[] deepKeys = newKey.split(DEEP_DICTIONARY_SEPARATOR_REGEX);
+
+			JSONObject deepDictionary = jsonObject;
+
+			for(int i=0;i<deepKeys.length-1;i++) {
+				if(deepDictionary.has(deepKeys[i])) {
+					try {
+						deepDictionary = deepDictionary.getJSONObject(deepKeys[i]);
+					} catch (JSONException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+			String key = deepKeys[deepKeys.length-1];
+
+			try {
+				Object value = deepDictionary.get(key);
+				if(value instanceof JSONArray) {
+					String oldValue = mPreferences.getString(newKey, null);
+					if(oldValue==null && !initial)
+						continue;
+					String newValue = ((JSONArray)value).toString();
+					if(!newValue.equals(oldValue)){
+						editor.putString(newKey,newValue);
+						changedKeys.add(newKey);
+					}
+				} else if(value instanceof String) {
+					String oldValue = mPreferences.getString(newKey, null);
+					if(oldValue==null && !initial)
+						continue;
+					String newValue = (String)value;
+					if(!newValue.equals(oldValue)){
+						editor.putString(newKey,newValue);
+						changedKeys.add(newKey);
+					}
+
+				} else if(value instanceof Integer) {
+					int oldValue = mPreferences.getInt(newKey, -1);
+					if(oldValue==-1 && !initial)
+						continue;
+					int newValue = ((Integer)value).intValue();
+					if(newValue != oldValue){
+						editor.putInt(newKey,newValue);
+						changedKeys.add(newKey);
+					}
+				}
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+		}
+		editor.apply();
+		//Let someone know we have a new value
+		for (int i = 0; i < changedKeys.size(); i++) {
+			Log.d(TAG, String.format(Locale.getDefault(), "Changed remote config value: %s", changedKeys.get(i)));
+			if(mListener!=null) {
+				mListener.onDownloadComplete(changedKeys.get(i));
+			}
+		}
+	}
+
+	private JSONObject initialFileToJsonObject() {
+		JSONObject remoteConfig = null;
+		InputStream is = null;
+		StringBuilder total = new StringBuilder();
+		try {
+			is = mContext.getResources().getAssets().open(REMOTE_CONFIG_FILE);
+			BufferedReader r = new BufferedReader(new InputStreamReader(is));
+			String line;
+			while ((line = r.readLine()) != null) {
+				total.append(line);
+			}
+			String jsonString = total.toString();
+			remoteConfig = new JSONObject(jsonString);
+		} catch (Exception e) {} finally {
+			if(is!=null) {
+				try {
+					is.close();
+				} catch (IOException e) {
+					e.printStackTrace();
 				}
 			}
 		}
-		// Replace the keys in availableMaps with the ones in the shared preferences
-		for(int i=0;i<keys.size();i++) {
-			String key = keys.get(i);
-			this.availableMaps.put(key,this.preferences.getString(key, null));
-		}
-		editor.commit();
+		return remoteConfig;
+	}
 
-		if(RemoteConfig.shouldUpdate(this.preferences, this.updateTime)) {
+	public void checkForUpdate() {
+		if(RemoteConfig.shouldUpdate(mPreferences, mUpdateTime)) {
 			// Fetch the config
-			new FetchConfigAsyncTask(this.context, this.configLocation, this.availableMaps).execute();
-		}
-	}
-
-	protected void setConfigLocation(String location) {
-		this.configLocation = location;
-		if(this.availableMaps != null) {
-			InitializeClass();
-		}
-	}
-
-	protected void setAvailableMaps(HashMap<String, String> map) {
-		this.availableMaps = map;
-		if(this.configLocation != null) {
-			InitializeClass();
+			new FetchConfigAsyncTask().execute();
 		}
 	}
 
@@ -93,95 +173,80 @@ public class RemoteConfig {
 	 * @return Returns the mapping for the parameter from the shared defaults 
 	 */
 	public String getString(String mapping) {
-		return this.preferences.getString(mapping, null);
+		checkForUpdate();
+		return mPreferences.getString(mapping, null);
 	}
 
 	public int getInt(String mapping) {
-		return this.preferences.getInt(mapping, -1);
+		checkForUpdate();
+		return mPreferences.getInt(mapping, -1);
 	}
 
 	private synchronized static boolean shouldUpdate(SharedPreferences preferences, long updateTime) {
-		long lastDownloadedConfig = preferences.getLong(RemoteConfig.LAST_DOWNLOADED_CONFIG_KEY, -1);
-		if(lastDownloadedConfig < 0 || System.currentTimeMillis() - lastDownloadedConfig > updateTime) {
+		long lastDownloadedConfig = preferences.getLong(RemoteConfig.LAST_DOWNLOADED_CONFIG_KEY, 0);
+		if(lastDownloadedConfig + updateTime < System.currentTimeMillis()) {
 			Editor editor = preferences.edit();
 			editor.putLong(RemoteConfig.LAST_DOWNLOADED_CONFIG_KEY, System.currentTimeMillis());
-			editor.commit();
+			editor.apply();
 			return true;
 		}
 		return false;
 	}
 
-	/**
-	 * Set how often this config should be updated
-	 * 
-	 * @param updateTime The update time in seconds
-	 */
-	public void setUpdateTime(long updateTime) {
-		this.updateTime = updateTime*1000;
-	}
-
-	public interface OnDownloadedConfigListener {
+	public interface RemoteConfigListener {
 		/**
 		 * This method is called when the config has been downloaded and it's values put into shared preferences
 		 * 
-		 * @param map The new map for value in shared preferences
-		 * @param value The new value for map in shared preferences
+		 * @param key The key for the new value in shared preferences
 		 */
-		public abstract void onDownloadedComplete(String map, String value);
+		public void onDownloadComplete(String map);
+	}
+
+	private ArrayList<String> getAllKeysFromJSONObject(JSONObject jsonObject, String prefix) {
+		ArrayList<String> allKeys = new ArrayList<String>();
+		Iterator<?> iter = jsonObject.keys();
+		while (iter.hasNext()) {
+			try {
+				String key = (String)iter.next();
+				Object value = jsonObject.get(key);
+				String newKey = null;
+				if(prefix!=null) {
+					newKey = prefix + DEEP_DICTIONARY_SEPARATOR + key;
+				} else {
+					newKey = key;
+				}
+				if(value instanceof JSONObject) {
+					allKeys.addAll(getAllKeysFromJSONObject(((JSONObject)jsonObject.get(key)), newKey));
+				} else {
+					allKeys.add(newKey);
+				}
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+		}
+		return allKeys;
 	}
 
 	// Allows the user to set an Listener and react to the event
-	public void setOnDownloadedConfigListener(OnDownloadedConfigListener listener) {
-		onDownloadedConfigListener = listener;
+	public void setRemoteConfigListener(RemoteConfigListener listener) {
+		mListener = listener;
 	}
 
-	private class FetchConfigAsyncTask extends AsyncTask<Void, Void, JSONArray>
-	{
-		private Context context;
-		private String configLocation;
-		private HashMap<String, String> availableMaps;
+	private class FetchConfigAsyncTask extends AsyncTask<Void, Void, JSONObject> {
 
-		public FetchConfigAsyncTask(Context context, String configLocation, HashMap<String, String> availableMaps) {
-			this.context = context;
-			this.configLocation = configLocation;
-			this.availableMaps = availableMaps;
+		@Override
+		protected JSONObject doInBackground(Void... params) {
+			return JSONMethods.readJSONFeed(mConfigLocation, null);
 		}
 
 		@Override
-		protected JSONArray doInBackground(Void... params) {
-			String feed = JSONMethods.readJSONFeed(configLocation, null, null);
-			JSONArray array = JSONMethods.parseJSON(feed);
-			return array;
-		}
-
-		@Override
-		protected void onPostExecute(JSONArray array) {
-			if(array != null) {
-				SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this.context);
-				Editor editor = settings.edit();
-				for(int i=0;i<array.length();i++) {
-					try {
-						JSONObject object = array.getJSONObject(i); // Object is the new values gotten from the internet
-						for (Map.Entry<String, String> entry : this.availableMaps.entrySet()) {
-							String key = entry.getKey();
-							String oldValue = entry.getValue();
-							if(object.has(key)) {
-								String newValue = object.getString(key);
-								if(oldValue == null || !newValue.equals(oldValue)){
-									editor.putString(key,newValue);
-									// Let someone know we have a new value
-									if(onDownloadedConfigListener!=null) {
-										onDownloadedConfigListener.onDownloadedComplete(key,newValue);
-									}
-								}
-							}
-						}					
-					} catch (JSONException e) {
-						e.printStackTrace();
-					}
-				}
-				editor.commit();
+		protected void onPostExecute(JSONObject config) {
+			if(config!=null) {
+				jsonObjectIntoPreferences(config, false);
+			} else {
+				Log.e(TAG, "Unable to read remote config");
 			}
 		}
 	}
 }
+
